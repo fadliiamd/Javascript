9ab4e7689800834ae9df9d0a29132d3d9c08fc92
Fadli <fadliadadakholqi15@gmail.com>
2022-12-07 22:17:36 +0700

    Default nilai pada parameter

---
+++ Beginner/Default.js
@@ -0,0 +1,8 @@
+function mintadata(x = 10) {
+    console.log(x);
+}
+mintadata();
+
+// Function di atas memiliki parameter dengan nilai default = 10,
+// maka ketika function tersebut dipanggil, apabila tidak ada pemberian nilai pada parameter yg telah di defined, 
+// maka parameter akan menampilkan nilai defaultnya = 10.
