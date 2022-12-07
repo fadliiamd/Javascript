1a1ec5be04851aa4cf563561a72eadb38d810f36
Fadli <fadliadadakholqi15@gmail.com>
2022-12-07 21:09:45 +0700

    Cara menggunakan return

--- Beginner/Function-return.js
+++ Beginner/Function-return.js
@@ -5,7 +5,7 @@ function ambil() {
 var tampung = ambil();
 console.log(tampung);
 
-//return berfungsi untuk mengembalikan nilai.
+//return berfungsi untuk mengembalikan nilai/value.
 //function di atas berisi mengenai function ambil yang memiliki variabel *lempar* dengan value ("Diterima"), dan
 //memanggil return untuk mengembalikan nilai/value pada variabel *lempar*.
 //lalu oleh variabel tampung diambil dengan cara memanggil function *ambil*.
