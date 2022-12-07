cae287fb5a60bb7db2b740503e3aa61f21b933b5
Fadli <fadliadadakholqi15@gmail.com>
2022-12-07 22:06:56 +0700

    Parameter

---
+++ Beginner/Parameter.js
@@ -0,0 +1,10 @@
+function mintadata(x, y, z) {
+    
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+mintadata("Hello", "Apakabar", "Sehat");
+
+//Parameter berfungsi untuk menampung nilai yang akan dikirimkan ke dalam sebuah function.
+// dan membuat function menjadi lebih dinamis.
