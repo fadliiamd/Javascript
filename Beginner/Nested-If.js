582ab9e0cb1abc6a6684629c4cf7cf2853bef35f
Fadli <fadliadadakholqi15@gmail.com>
2022-12-08 13:19:42 +0700

    Nested If Condition

---
+++ Beginner/Nested-If.js
@@ -0,0 +1,18 @@
+var x = 35;
+var y = 30;
+var z = 40;
+
+if (x > y) {
+    if (x > z) {
+        console.log("X adalah yang paling besar")
+    } else {
+        console.log("X adalah paling besar kedua")
+    }
+}
+else {
+    if (x < z) {
+        console.log("X adalah yang paling kecil")
+    } else {
+        console.log("X adalah paling kecil kedua")
+    }
+}
