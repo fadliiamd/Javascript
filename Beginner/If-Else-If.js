9c56c6f7b81bb514427495f182caf1d200b257e7
Fadli <fadliadadakholqi15@gmail.com>
2022-12-08 13:00:09 +0700

    If Else If Condition

---
+++ Beginner/If-Else-If.js
@@ -0,0 +1,14 @@
+var x = 41;
+
+if (x > 40) {
+    console.log("Top global Gusion")
+} else if (x > 20) {
+    console.log("Top global Kadita")
+} else if (x > 10) {
+    console.log("Top global Beatrix")
+} else {
+    console.log("Top global Lancelot")
+}
+
+//if else if merupakan variasi else if yang memiliki banyak kondisi, seperti else di atas
+// else if di atas memiliki 4 kondisi, x lebih dari 40, x lebih dari 20, x lebih dari 10, dan terakhir selain dari ketiganya.
