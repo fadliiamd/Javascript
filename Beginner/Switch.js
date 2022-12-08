e595d7d26eeb807d89af9c89de6022cd3d5b4335
Fadli <fadliadadakholqi15@gmail.com>
2022-12-09 00:10:51 +0700

    Switch

---
+++ Beginner/Switch.js
@@ -0,0 +1,21 @@
+function testingSwitch() {
+    var nilai = 10;
+
+    switch (nilai) {
+        case 10:
+            console.log("Sangat Baik");
+            break;
+        case 8:
+            console.log("Baik");
+            break;
+        case 6:
+            console.log("Cukup");
+            break;
+        default:
+            console.log("Lainnya")
+    }
+}
+
+testingSwitch()
+
+//Switch berguna untuk menghandling nilai sebuah statement.
