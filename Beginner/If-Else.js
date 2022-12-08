980d1a31730c71a4d979145883082158fd6c4de8
Fadli <fadliadadakholqi15@gmail.com>
2022-12-08 12:44:14 +0700

    If Else Condition

---
+++ Beginner/If-Else.js
@@ -0,0 +1,19 @@
+function streetOne() {
+    var nilai = 3;
+    if (nilai === 3) {
+        console.log("Benar")
+    } else if (nilai === 2) {
+        console.log("Dua")
+    } else {
+        console.log("Salah")
+    }
+}
+
+function streetTwo() {
+    var tinggi = 3;
+
+    if (tinggi === 3) console.log("OK")
+    else console.log("Bukan")
+}
+streetOne()
+streetTwo()
