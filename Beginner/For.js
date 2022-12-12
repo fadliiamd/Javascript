ec9fe2f8cd5810331f352f52cd24e942a6d8c9a0
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 09:09:44 +0700

    Perulangan for

--- Beginner/For.js
+++ Beginner/For.js
@@ -7,5 +7,5 @@ function perulangan() {
 perulangan()
 
 //for merupakan fungsi perulangan, kondisi di atas adalah fungsi perulangan
-// yang menjalankan var i dengan value 1 akan berulang selama var i belum mendapatkan value 1,
+// yang menjalankan var i dengan value 1 akan berulang selama var i belum mendapatkan value 11,
 // perulangan akan berhenti apabila kurang dari 11, artinya perulangan akan berhenti ketika value = 10
