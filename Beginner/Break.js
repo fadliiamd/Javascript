81131a22beebf52d100ede696bd86947306a6dad
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 10:08:22 +0700

    Break

---
+++ Beginner/Break.js
@@ -0,0 +1,9 @@
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+    if (i == 7)
+        break;
+}
+
+// Fungsi break, merupakan fungsi untuk menghentikan eksekusi
+// contoh pada kondisi d atas, break akan berfungsi apabila perulangan sudah mencapai == 7
+// maka ekseskusi selesai, atau dengan kata lain tidak akan dilanjutkan melebihi = 7.
