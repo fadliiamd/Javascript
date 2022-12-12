ab8b90aca7ea2781fddc266cdc9fffe95ccf9881
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 09:58:39 +0700

    Perulangan Infinity while (while infinity loop

---
+++ Beginner/Infinity-while.js
@@ -0,0 +1,9 @@
+var i = 1;
+
+while (true) {
+    console.log(i);
+    i++;
+}
+
+// Infinity while merupakan salah satu jenis perulangan infinity,
+// perbedaan dari infinity for hanya terletak dari cara penulisannya saja.
