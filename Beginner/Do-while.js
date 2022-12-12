20c9a05024c1bb3a7423c01ea4335ca4d4d294dc
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 09:26:26 +0700

    Perulangan Do While

---
+++ Beginner/Do-while.js
@@ -0,0 +1,10 @@
+var i = 10;
+
+do {
+    console.log("Ini yang ke ", i);
+    i++;
+} while (i < 10)
+
+// perbedaan dari while dengan do while yaitu, while mengeksekusi kondisi dengan-
+// mengecek kondisi terlebih dahulu, baru menampilkan statement.
+// sementara do while, menampilkan statement terlebih dahulu, baru selanjutnya mengecek kondisi.
