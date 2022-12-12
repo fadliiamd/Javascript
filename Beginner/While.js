d7c051748b9fffbe85cfb7d7de0dd5f5874e898e
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 09:19:00 +0700

    Perulangan while

---
+++ Beginner/While.js
@@ -0,0 +1,11 @@
+var i = 0;
+
+while (i < 10) {
+    console.log("Ini urutan ke", i);
+    i++;
+}
+
+// While merupakan salah satu jenis kondisi perulangan,
+// kondisi di atas menyatakan variabel i dengan value = 0,
+// lalu kondidi perulangan menyatakan bahwa i akan berhenti melakukan perulangan
+// apabila value sudah mencapai 10.
