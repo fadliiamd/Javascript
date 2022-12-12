20072652ea51711560ad5cacaa08076f53018b89
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 09:55:02 +0700

    Perulangan Infinity for (for infinity loop)

---
+++ Beginner/Infinity-for.js
@@ -0,0 +1,6 @@
+for (var i = 0; i >= 0; i++) {
+    console.log(i);
+}
+
+// infinity for adalah salah satu kondisi perulangan yang akan terus berulang
+// infinity fo ini hanya akan berhenti apabila kita sendiri yang memberhentikan perulangan tersebut, karena kondisinya akan terus teroenuhi.
