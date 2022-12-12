da67d5ac78d71c003ba3dd9e293e9fd3447aa981
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 10:12:25 +0700

    Continue

---
+++ Beginner/Continue.js
@@ -0,0 +1,7 @@
+for (var i = 0; i <= 10; i++) {
+    if (i == 7)
+        continue;
+    console.log(i);
+}
+
+// Continue berarti tidak akan mengeksekusi = 7 dan akan melanjutkan langsung ke value selanjutnya (8++) sampai kondisi terpenuhi (<=0).
