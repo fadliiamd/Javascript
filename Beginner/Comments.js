fe0de0d5277a5e304362fd8143e869a665037db9
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 21:39:43 +0700

    Comments

---
+++ Beginner/Comments.js
@@ -0,0 +1,13 @@
+function testComments() {
+    console.log("satu");
+    console.log("dua");
+    //console.log("tiga");
+
+    console.log("empat");
+    console.log("lima");
+    /*console.log("enam");*/
+}
+
+// Cara melakukan comment bisa menggunakan dua cara
+// Pertama bisa menggunakan double slash atau garis miring "//"
+// Kedua bisa menggunakan garis miring dan bintang "/*test*/"
