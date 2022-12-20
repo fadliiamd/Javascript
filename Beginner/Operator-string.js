6288db5bab845f8cff21d505da1ee1bf977f01c3
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 23:04:20 +0700

    Operator string

---
+++ Beginner/Operator-string.js
@@ -0,0 +1,9 @@
+var x = 'Hai';
+var y = 'Atha';
+var angka = 10;
+var z = x + y + angka;
+
+console.log(z);
+
+// Operator string, apabila terdapat type data number yang di jumlahkan dengan string
+// maka akan menjadi string
