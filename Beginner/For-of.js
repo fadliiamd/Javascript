df68ee2015205e2b1a7e215c43a01875eec86ad5
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 08:39:05 +0700

    For Of

---
+++ Beginner/For-of.js
@@ -0,0 +1,8 @@
+var angka = [4, 2, 4, 5, 7, 9];
+for (let x of angka) {
+    console.log(x);
+}
+
+// merupakan metode pemanggilan array tanpa harus menyebutkan index([0])
+// of akan menyimpan nilai variabel angka ke dalam x.
+// sehingga kita hanya harus memanggil variabel x.
