139bc5bfa0cb8714381461522a7bc68beea4624d
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 13:21:10 +0700

    Tambah property

---
+++ Beginner/Property2.js
@@ -0,0 +1,19 @@
+// Tambah property
+
+function panggilMobil() {
+    var mobil = {
+        brand: 'rolls royce',
+        warna: 'hitam doff',
+        harga: 20000,
+    }
+    // cara merubah nilai property
+    mobil.tahun = 2019
+    console.log(mobil);
+}
+
+panggilMobil()
+
+// Untuk menambah property cukup mengetikkan nama varibael
+// lalu diikuti dengan nama property yg mau ditambahkan dan isikan nilainya, contoh :
+// mobil.tahun = 2019;
+// kodingan tersebut berarti menambahkan property tahun kepada variabel mobil dengan nilai atau value 2019.
