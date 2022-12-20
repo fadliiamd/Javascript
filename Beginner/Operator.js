ec037e773bdba752973b49f03380e38acc03b00d
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 22:25:48 +0700

    Operator

---
+++ Beginner/Operator.js
@@ -0,0 +1,11 @@
+function Operator() {
+    var a = 3;
+    var b = 4;
+
+    return a + b;
+}
+
+console.log(Operator());
+
+// Terdapat berbagai macam jenis operator, diantaranya
+// penjumlahan (+), pengurangan (-), pembagian (/), dan perkalian (*)
