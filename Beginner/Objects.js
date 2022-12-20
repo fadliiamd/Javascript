8909b526f4d9385b11a8ad36bf4febf02b606dbf
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 11:05:26 +0700

    Objects

---
+++ Beginner/Objects.js
@@ -0,0 +1,16 @@
+function panggilMobil() {
+    var mobil = {
+        brand: 'rolls royce',
+        warna: 'hitam doff',
+        harga: 20000,
+    }
+
+    console.log(mobil);
+    console.log(mobil.brand);
+}
+
+panggilMobil()
+
+// Object merupakan salah satu jenis type data,
+// yang tidak hanya sekedar menyimpan data, melainkan juga memberikan informasi data tersebut.
+// contoh menyimpan data mobil dengan informasi brand, warna, dan harganya.
