3d9179f1e0a7d88d17dcaf851cbf3d6d8d0b4f60
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 12:52:23 +0700

    Array object

---
+++ Beginner/Array-objects.js
@@ -0,0 +1,15 @@
+function panggilMobil() {
+    var mobil = {
+        brand: 'rolls royce',
+        warna: 'hitam doff',
+        harga: 20000,
+        tahun: [2001, 1998, 2005, 2007],
+    }
+
+    console.log(mobil.tahun[2]);
+}
+
+panggilMobil()
+
+// Array object merupakan data array yang terdapat di dalam object,
+// mekanisme pemanggilan array tetap sama.
