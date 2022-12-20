e9c7a1c10411510414082f255a3e4f5f2fba4f28
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 22:31:55 +0700

    Operator aritmatika

---
+++ Beginner/Operator-aritmatika.js
@@ -0,0 +1,11 @@
+var a= 100;
+var b = 10;
+
+console.log(a + b); // Penjumlahan
+console.log(a - b); // Pengurangan
+console.log(a * b); // Perkalian
+console.log(a / b); // Pembagian
+console.log(a % b); // Sisa hasil bagi
+
+// Operator aritmatika terdiri dari penjumlahan (+), pengurangan (-)
+// Perkalian (*), pembagian (/), dan sisa hasil bagi (%)
