984c478102c4e927e66182004cac6017e317b634
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 22:46:29 +0700

    Operator perbandingan

---
+++ Beginner/Operator-perbandingan.js
@@ -0,0 +1,11 @@
+var x = 10;
+var y = 11;
+
+console.log(x>y) // Lebih besar dari
+console.log(x<y) // Kurang dari
+console.log(x>=y) // Lebih besar sama dengan dari
+console.log(x<=y) // Kurang dari sama dengan
+console.log(x==y) // Sama dengan
+console.log(x===y) // Sama dengan type data sama
+console.log(x!=y) // Tidak sama dengan
+console.log(x!==y) // Tidak sama dengan type data sama
