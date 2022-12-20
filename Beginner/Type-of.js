1d793dbc562936e89e369fceaef282e71eb5cd79
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 21:34:02 +0700

    Type of

---
+++ Beginner/Type-of.js
@@ -0,0 +1,13 @@
+function checktypedata() {
+    var random = {
+        nama: 'abdul',
+    }
+
+    console.log(typeof(random))
+}
+
+checktypedata()
+
+// typeof merupakan fungsi untuk mengecek type data sebuah variabel
+// contoh pada variabel random d atas merupakan sebuah type data object
+// maka ketika dilakukan console.log akan tampil type data object
