25a864b6ed28d6775fdebf1935aeca0e9636feac
Fadli <fadliadadakholqi15@gmail.com>
2022-12-20 22:54:08 +0700

    Operator logika

---
+++ Beginner/Operator-logika.js
@@ -0,0 +1,9 @@
+var a = 10;
+
+console.log(a > 5 && a < 20); // And
+console.log(a > 20 || a < 5); // Or
+console.log(!false); // Not (Negasi)
+
+// and adalah kondisi dimana semua syarat harus terpenuhi agar menghasilkan true
+// or adalah kondisi dimana minimal hanya satu yang terpenuhi agar menghasilkan true
+// negasi adalah kebalikan dari nilai yang dideklarasikan, apabila !true maka akan menghasilkan false
