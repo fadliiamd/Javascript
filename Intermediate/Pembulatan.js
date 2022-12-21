2e190e38c3c6f4982f9e0444df9758897878b36b
Fadli <fadliadadakholqi15@gmail.com>
2022-12-21 23:49:24 +0700

    Ceil, Floor, dan Round

---
+++ Intermediate/Pembulatan.js
@@ -0,0 +1,10 @@
+var x = 15.7;
+
+console.log(Math.ceil(x));
+console.log(Math.floor(x));
+console.log(Math.round(x));
+
+// Ceil adalah pembulatan ke atas
+// floor adalah pembulatan ke bawah
+// round adalah pembulatan berdasarkan angka 5, apabila lebih dari 5 maka akan dilakukan pembulatan ke atas
+// sedangkan apabila kurang dari 5 maka akan dilakukan pembulatan ke bawah
