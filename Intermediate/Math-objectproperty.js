cacd39e7978433a102e64317f1b4d6e64ac97e83
Fadli <fadliadadakholqi15@gmail.com>
2022-12-21 23:30:25 +0700

    Math object property

---
+++ Intermediate/Math-objectproperty.js
@@ -0,0 +1,8 @@
+console.log(Math.E) // angka logaritma natural e
+console.log(Math.LN10) // angka logaritma narural 10
+console.log(Math.LN2) // angka logaritma natural 2
+console.log(Math.LOG10E) // angka logaritma natural e basis 10
+console.log(Math.LOG2E) // angka logaritma natural e basis 2
+console.log(Math.PI) // berisi angka pi 3.14xxx
+console.log(Math.SQRT1_2) // angka 1 dibagi dengan akar kuadrat 2
+console.log(Math.SQRT2) // angka akar kuadrat 2
