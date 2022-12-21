193f9a44389e12a809df3f5cf2b8a41bd3e8b646
Fadli <fadliadadakholqi15@gmail.com>
2022-12-22 00:17:57 +0700

    Math min max

---
+++ Intermediate/Math-minmax.js
@@ -0,0 +1,13 @@
+function minmax() {
+    
+    var a = Math.min(21, 3, 45, 35, 1);
+    console.log(a);
+
+    var b = Math.max(21, 3, 45, 35, 1);
+    console.log(b);
+}
+
+minmax()
+
+// min untuk menampilkan data paling minimal
+// max untuk menampilkan data paling maksimal
