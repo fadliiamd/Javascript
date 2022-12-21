31e18b455e9579c054fcc5bd2393116cd404b794
Fadli <fadliadadakholqi15@gmail.com>
2022-12-21 13:06:06 +0700

    Array splice

--- Intermediate/Array-splice.js
+++ Intermediate/Array-splice.js
@@ -11,4 +11,4 @@ console.log(panggilSplice())
 // Fungsi splice dapat untuk menyisipkan atau memasukkan sebuah data ke dalam sebuah array sesuai dengan keinginan atau kebutuhan
 // kota.splice(2,0, "") 2 berarti untuk menempatkan data di index ke 2 ["Maghribi"], dan 0 berarti menyisipkan data tanpa menghapus data pada index tersebut
 // apabila 0 nya di ganti menjadi 1, maka artinya menghapus data index pada array hendak yang disisipkan
-// apabila 0 nya diganti menjadi 2, maka artinya menghapus data index pada array dan 1 index setelahnya, danseterusnya.
+// apabila 0 nya diganti menjadi 2, maka artinya menghapus data index pada array dan 1 index setelahnya, dan seterusnya.
