d95958558918cff9817fff1243e80d799b938ab2
Fadli <fadliadadakholqi15@gmail.com>
2022-12-22 00:03:18 +0700

    Math absolute (abs)

---
+++ Intermediate/Math-abs.js
@@ -0,0 +1,7 @@
+var x = 20;
+var y = 30;
+
+console.log(Math.abs(x - y))
+
+// abs atau absolute berfungsi untuk membuat suatu nilai menjadi mutlak  atau absolute
+// meskipun nilai nya negatif, tetap akan dianggap positif (absolut)
